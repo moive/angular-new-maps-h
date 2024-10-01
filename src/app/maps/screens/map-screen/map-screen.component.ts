6cccb83e656a2eaebc09347eba9c7086840b4e60
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-10-01 12:05:48 -0500

    get geolocation user

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-screen',
   templateUrl: './map-screen.component.html',
-  styleUrl: './map-screen.component.scss'
+  styleUrl: './map-screen.component.scss',
 })
 export class MapScreenComponent {
-
+  constructor(private placesService: PlacesService) {}
 }
---
+++ src/app/maps/services/places.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlacesService {
+  userLocation?: [number, number];
+
+  get isUserLocationReady(): boolean {
+    return !!this.userLocation;
+  }
+  constructor() {
+    this.getUserLocation();
+  }
+
+  async getUserLocation(): Promise<[number, number]> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          this.userLocation = [coords.longitude, coords.latitude];
+          resolve(this.userLocation);
+        },
+        (err) => {
+          alert("Couldn't get geolocation location");
+          console.log(err);
+          reject();
+        }
+      );
+    });
+  }
+}
