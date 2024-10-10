4004abae40b98a774428db3716302a054cbb8f86
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-10-09 23:43:37 -0500

    draw polylines

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
-import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
-import { Feature } from '../interfaces/places.interface';
+import {
+  LngLatBounds,
+  LngLatLike,
+  Map,
+  Marker,
+  Popup,
+  SourceSpecification,
+} from 'mapbox-gl';
+import { Feature, Properties } from '../interfaces/places.interface';
 import { DirectionsApiClient } from '../api';
 import { DirectionsResponse, Route } from '../interfaces/directions.interface';
 
@@ -83,5 +90,45 @@ export class MapService {
     });
 
     this.map.fitBounds(bounds, { padding: 200 });
+
+    // Polyline
+
+    const sourceData: SourceSpecification = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: coords,
+            },
+          },
+        ],
+      },
+    };
+
+    if (this.map.getLayer('RouteString')) {
+      this.map.removeLayer('RouteString');
+      this.map.removeSource('RouteString');
+    }
+
+    this.map.addSource('RouteString', sourceData);
+
+    this.map.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout: {
+        'line-cap': 'round',
+        'line-join': 'round',
+      },
+      paint: {
+        'line-color': 'black',
+        'line-width': 3,
+      },
+    });
   }
 }
